dbfe89b8cb7f610e2a798340afef21484d222595
JustDevOzy <gracendimele5@gmail.com>
2023-08-02 15:20:13 +0100

    done

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,17 +1,15 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-comparison = " "
 
+if last_digit > 5:
+    result = "and is greater than 5"
 
-
-if last_digit > 5 :
-    comparison = "and is greater than 5"
-
-elif last_digit == 0 :
-    comparison = "and is zero"
+elif last_digit == 0:
+    result = "and is 0"
 
 else:
-    comparison ="and is less than 6 and not zero"
+    result = "and is less than 6 and not 0"
 
-print(f"Last digit of {number} is {last_digit}, {comparison} ")
+
+print(f"Last digit of {number} is {last_digit} {result}")
