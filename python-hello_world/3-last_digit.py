60575f405e48e6a18e378e5be94009d8c928bd82
JustDevOzy <gracendimele5@gmail.com>
2023-08-02 15:32:25 +0100

    .......

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,8 +5,8 @@ last_digit = abs(number) % 10
 print("Last digit of", number, "is", last_digit, end=" ")
 
 if last_digit > 5:
-    print("positive")
+    print("and is greater than 5")
 elif last_digit == 0:
-    print("negative")
+    print("and is 0")
 else:
     print("and is less than 6 and not 0")
